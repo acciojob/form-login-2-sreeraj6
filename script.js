2c8e14a7c931b3fa45523294c84e1c359765cebc
SREERAJ R <84067685+sreeraj6@users.noreply.github.com>
2023-04-02 10:38:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ function form_alert() {
 	var lname = document.getElementById("lname").value;
 	var phone = document.getElementById("phone").value;
 	var email = document.getElementById("email").value;
+	fname = "First Name: "+fname;
+	lname = "Last Name: "+lname;
+	phone = "Phone Number: "+phone;
+	email = "Email ID: "+email;
 	alert(fname+"\n"+lname+"\n"+phone+"\n"+email);
 }
